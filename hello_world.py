49ce3ae18d1e04e8e3df78d4e931ea7b11a46cd2
Yash Chaube <64903635+pho3nix3@users.noreply.github.com>
2020-06-02 19:25:12 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -7,4 +7,11 @@ i am excited to start a new journey
 print(" My name is pho3nix ")
 print("a.k.a. Yash")
 
+print("Hobbies")
+print('''>>football
+>>technology
+>>art
+>>gaming
+>>singing''')
+
 print("THANK YOU")
